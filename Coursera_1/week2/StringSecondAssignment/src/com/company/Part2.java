5f5c04daede5978a2b4b31177d37cc2b629c5497
ChandraShekarPasumarth <chandrashekar.pasumarthi@zemosolabs.com>
2021-07-13 10:56:47 +0530

    Coursera1 week2 assignment2 completed

---
+++ Coursera_1/week2/StringSecondAssignment/src/com/company/Part2.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Part2 {
+    public int howMany(String stringa, String stringb){
+        int totalOccurences=0;
+        int start=0;
+        while(true) {
+            int index = stringb.indexOf(stringa, start);
+            if (index != -1) {
+                totalOccurences++;
+                start = index + stringa.length();
+            } else {
+                break;
+            }
+        }
+        return totalOccurences;
+    }
+    public void testHowMany(){
+        String stringa = "GAA";
+        String stringb = "ATGAACGAATTGAATC";
+        System.out.println("Total occurences of " + stringa + " in " + stringb + " = " + howMany(stringa, stringb));
+
+        stringa = "AA";
+        stringb = "ATAAAA";
+        System.out.println("Total occurences of " + stringa + " in " + stringb + " = " + howMany(stringa, stringb));
+
+    }
+}
