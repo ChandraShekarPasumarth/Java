dcecfadef989376be889a7c0d96f222cf74ee487
ChandraShekarPasumarth <chandrashekar.pasumarthi@zemosolabs.com>
2021-07-15 13:17:35 +0530

    Coursera2 completed

---
+++ Coursera_2/src/com/company1/Logs/Main.java
@@ -0,0 +1,15 @@
+package com.company1.Logs;
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]){
+        Tester t =new Tester();
+        t.testCountUniqueIPsInRange();
+        t.testLogAnalyzer();
+        t.testLogEntry();
+        t.testPrintAllHigherThanNum();
+        t.testUniqueIP();
+        t.testUniqueIPVisitsOnDay();
+
+    }
+}
